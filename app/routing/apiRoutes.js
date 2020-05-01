4eca4afe3afae6b7f1c59217258cb6d09f1fbb7a
Zach S.L <zach.s.ledford@gmail.com>
2020-04-30 23:56:47 -0400

    added questions text

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 var friendsData = require("../data/friends.js");
 
 module.exports = function(app){
-    app.get("api/friends", function(req, res){
+    app.get("/api/friends", function(req, res){
         res.json(friendsData);
     });
 };
