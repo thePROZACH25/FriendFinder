af680f79cb3b8fb3a5a1fddfb847cdee504b0d44
Zach S.L <zach.s.ledford@gmail.com>
2020-04-30 21:43:14 -0400

    PORT connecting

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes")(app);
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
